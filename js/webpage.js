1c03b494e729e5b25a19591c4b88ffbeb3438122
Your Name <you@example.com>
2017-10-09 23:37:51 +0100

    burger and menu animation, logo, github link

---
+++ js/webpage.js
@@ -0,0 +1,26 @@
+function webpageJS() {
+
+    var self = this;
+    var menu = document.getElementsByClassName('menu-nav')[0];
+    var menuNavigation = menu.getElementsByTagName('nav')[0];
+    var menuButton = document.getElementById('menuButton');
+    var menuAnimations = document.getElementsByClassName('menu-animated');
+
+    /* Callbacks */
+    function toggleMenu() {
+        menuNavigation.classList.toggle('menu-animated');
+        menuButton.classList.toggle('open');
+    }
+
+    /* Init Callbacks */
+    function initCallbacks(){
+
+        menuButton.addEventListener('click', toggleMenu);
+
+    }
+
+    initCallbacks();
+
+}
+
+webpageJS();
