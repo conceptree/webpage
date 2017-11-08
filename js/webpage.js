d89867878c4b0e6af426d5064a2b010d6c749d63
Your Name <you@example.com>
2017-11-08 22:15:32 +0000

    projects section

--- js/webpage.js
+++ js/webpage.js
@@ -1,31 +1,68 @@
+"use strict";
 function webpageJS() {
+  var self = this;
+  var menu = document.getElementsByClassName("menu-nav")[0];
+  var menuNavigation = menu.getElementsByTagName("nav")[0];
+  var menuButton = document.getElementById("menuButton");
+  var menuAnimations = document.getElementsByClassName("menu-animated");
+  var loginBtn = document.getElementById("loginBtn");
+  var loginWindow = document.getElementsByClassName("login-window")[0];
+  var mainWrapper = document.getElementsByClassName("main-wrapper")[0];
+  var menuBtnsList = [
+    { id: "homeBtn", content: "Home" },
+    { id: "aboutBtn", content: "Sobre" },
+    { id: "projectsBtn", content: "Projectos" },
+    { id: "contactsBtn", content: "Contactos" }
+  ];
 
-    var self = this;
-    var menu = document.getElementsByClassName('menu-nav')[0];
-    var menuNavigation = menu.getElementsByTagName('nav')[0];
-    var menuButton = document.getElementById('menuButton');
-    var menuAnimations = document.getElementsByClassName('menu-animated');
-    var loginBtn = document.getElementById('loginBtn');
-    var loginWindow = document.getElementsByClassName('login-window')[0];
+  /* Callbacks */
+  function toggleMenu() {
+    menuNavigation.classList.toggle("menu-animated");
+    menuButton.classList.toggle("open");
+  }
 
-    /* Callbacks */
-    function toggleMenu() {
-        menuNavigation.classList.toggle('menu-animated');
-        menuButton.classList.toggle('open');
+  function readScroll() {
+    switch (window.scrollY) {
     }
+  }
 
-    function toggleLogin(){
-        loginWindow.classList.toggle('login-window-on');
+  function menuActions(event) {
+    menuBtnsList.forEach(function(el, idx){
+        var pageSections = document.getElementsByTagName('section');
+        if(event.target.id === el.id){
+            if(idx === 0){
+                window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+            }else{
+                idx--;
+                window.scroll({ top: pageSections[idx].offsetTop, left: 0, behavior: 'smooth' });
+            }
+        }
+    });
+  }
+
+  function toggleLogin() {
+    loginWindow.classList.toggle("login-window-on");
+  }
+
+  /* Init */
+  function initCallbacks() {
+    menuButton.addEventListener("click", toggleMenu);
+    loginBtn.addEventListener("click", toggleLogin);
+    window.addEventListener("scroll", readScroll);
+    menuNavigation.addEventListener("click", menuActions);
+  }
+
+  function buildMenu() {
+    for (var el in menuBtnsList) {
+      var menuElement = document.createElement("a");
+      menuElement.id = menuBtnsList[el].id;
+      menuElement.innerText = menuBtnsList[el].content;
+      menuNavigation.appendChild(menuElement);
     }
+  }
 
-    /* Init Callbacks */
-    function initCallbacks(){
-        menuButton.addEventListener('click', toggleMenu);
-        loginBtn.addEventListener('click', toggleLogin);
-    }
-
-    initCallbacks();
-
+  initCallbacks();
+  buildMenu();
 }
 
 webpageJS();
